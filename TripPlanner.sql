49386155b0c6be5f7c9ddd2c53be0b122be68b64
Kendall <kendallallmand@gmail.com>
2020-05-17 16:29:03 -0400

    first

---
+++ TripPlanner.sql
@@ -0,0 +1,39 @@
+--CREATE DATABASE TripPlanner;
+--USE TripPlanner;
+
+--CREATE TABLE [User](
+--Id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
+--[Name] NVARCHAR(50),
+--[Address] NVARCHAR(50),
+--PhoneNumber NVARCHAR(50),
+--DOB NVARCHAR(50)
+--); 
+
+--CREATE TABLE Passport(
+--Id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
+--PassportNumber NVARCHAR(50),
+--[Address] NVARCHAR(50),
+--EXPDate NVARCHAR(50)
+--); 
+
+--CREATE TABLE Trip(
+--Id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
+--Budget INT,
+--StartDest NVARCHAR(50),
+--EndDest NVARCHAR(50),
+--ModeOfTravel NVARCHAR(50),
+--StartDate NVARCHAR(50),
+--EndDate NVARCHAR(50)
+--); 
+
+--CREATE TABLE Hotel(
+--Id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
+--CostPerNight INT,
+--ContactAdd NVARCHAR(50),
+--ContactNum NVARCHAR(50),
+--CheckInDate NVARCHAR(50),
+--CheckOutDate NVARCHAR(50)
+--); 
+                                   
+
+
